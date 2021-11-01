d3106cb15c897a5e6025e1251e2368d5fa7f2196
Hayate Nakao <hayatemomizi@icloud.com>
2021-11-01 21:41:23 +0900

    Update section 2-q2

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -10,9 +10,9 @@ function setup() {
       else{fill(128, 128, 128)}
     rect(size*i, size*j, size, size);
 {
-  if(j<3 & (i+j)%2==1){fill(255, 0, 0);}
+  if(j<3 && (i+j)%2==1){fill(255, 0, 0)}
 else if(j<5){noFill();}
-else if(j<8 & (i+j)%2==1){fill(0,0,0)}
+else if(j<8 && (i+j)%2==1){fill(0,0,0)}
 else{noFill()}
   circle(size*(i+1/2), size*(j+1/2), size*4/5);
 }
